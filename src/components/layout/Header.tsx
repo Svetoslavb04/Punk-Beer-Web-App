d80066d2ca7c2dcda17b54c1c355e8733173fb05
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-27 16:24:33 +0300

    feat: Update Header text

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,8 +3,8 @@ import { Link, NavLink } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="w-100 d-flex justify-content-between align-items-center text-light">
-      <Link to="/" className="fs-2">
-        Punk Beer
+      <Link to="/" className="fs-2 fw-light">
+        Beans Love Beers
       </Link>
       <nav className="d-flex align-items-center gap-4 fs-6">
         <NavLink to="/">Home</NavLink>
