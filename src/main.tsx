604f5430dbb9781e84d251e2086f61117b46c56f
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-27 15:11:41 +0300

    feat: import bootstrap-utilities

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import App from './components/App.tsx';
 
+import './style/style.scss';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
