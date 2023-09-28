66d64f7555993e30c526cd3a461052a836baada3
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-28 15:18:12 +0300

    refactor: rename routes to pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Router from './routes/Router.tsx';
+import Router from './pages/Router.tsx';
 
 import './style/style.scss';
 
