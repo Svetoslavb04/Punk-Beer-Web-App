9ffb2797b6c87bd981284c999ff09734e5b74dc4
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-29 16:12:51 +0300

    feat: provide beer in BeerCard events

--- src/components/ui/BeerCard/index.tsx
+++ src/components/ui/BeerCard/index.tsx
@@ -1,6 +1,6 @@
 import './BeerCard.scss';
 
-import { FC } from 'react';
+import { FC, MouseEvent } from 'react';
 import { Star, StarFill } from 'react-bootstrap-icons';
 
 import { Beer } from '../../../interfaces/Beer';
@@ -9,23 +9,29 @@ interface Props {
   className?: string;
   starred?: boolean;
   beer: Beer;
-  onStarClick?: React.MouseEventHandler<HTMLSpanElement>;
-  onBeerImageClick?: React.MouseEventHandler<HTMLImageElement>;
+  onStarClick?: (beer: Beer, event: MouseEvent<HTMLSpanElement>) => void;
+  onBeerImageClick?: (beer: Beer, event: MouseEvent<HTMLImageElement>) => void;
 }
 
 const BeerCard: FC<Props> = ({ className, starred, beer, onStarClick, onBeerImageClick }) => {
   return (
-    <div className={`beer-card p-2 border rounded-4 row overflow-hidden ${className}`}>
+    <div
+      beer-id={beer.id}
+      className={`beer-card p-2 border rounded-4 row overflow-hidden ${className}`}
+    >
       <div className="mh-100 col-4 py-4 px-3 d-flex flex-column justify-content-center">
         <img
           src={beer.imageUrl}
           alt="beer"
-          onClick={onBeerImageClick}
+          onClick={onBeerImageClick?.bind(this, beer)}
           className="object-fit-contain mw-100 mh-100 h-auto"
         />
       </div>
       <div className="col-8 h-100 my-2 d-flex flex-column">
-        <span onClick={onStarClick} className="star-container d-flex align-self-end">
+        <span
+          onClick={onStarClick?.bind(this, beer)}
+          className="star-container d-flex align-self-end"
+        >
           {starred ? <StarFill title="star-fill-icon" /> : <Star title="star-icon" />}
         </span>
         <h3 className="mt-1 fs-6">{beer.name}</h3>
