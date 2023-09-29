a1b8f20f6ac6cd828f7194777c145ae0feb69eac
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-29 18:17:19 +0300

    feat: implement beer search in Index page

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,4 +1,13 @@
+import './Index.scss';
+
+import { ChangeEvent, useState } from 'react';
+
 import { useSearchParams } from 'react-router-dom';
+
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+
 import BeersList from './components/BeersList';
 
 const Index = () => {
@@ -7,17 +16,42 @@ const Index = () => {
     per_page: '6',
   });
 
+  const [beerName, setBeerName] = useState(searchParams.get('beerName') || '');
+
+  const handleBeerSearchChange = (e: ChangeEvent<HTMLInputElement>) => setBeerName(e.target.value);
+
+  const handleBeerSearch = () => {
+    setSearchParams(prev => {
+      prev.set('beerName', beerName);
+
+      return prev;
+    });
+  };
+
   return (
     <div>
       <div className="container mt-5">
+        <div id="search-beer-container" className="my-4 mx-auto">
+          <InputGroup>
+            <FormControl
+              placeholder="Search for beer..."
+              aria-label="Search for beer..."
+              aria-describedby="Search for beer"
+              value={beerName}
+              onChange={handleBeerSearchChange}
+            />
+            <Button variant="outline-secondary" onClick={handleBeerSearch}>
+              Search
+            </Button>
+          </InputGroup>
+        </div>
         <div className="row gy-4 gx-5">
           <BeersList
-            search=""
+            search={searchParams.get('beerName') || ''}
             page={Number(searchParams.get('page')) || 1}
             perPage={Number(searchParams.get('perPage')) || 6}
           />
         </div>
-        <div></div>
       </div>
     </div>
   );
