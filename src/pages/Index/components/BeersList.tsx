39d10c5a9f9a8ec9678f4a5c401c162ca917e04e
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-29 17:24:42 +0300

    feat: add BeersList component

---
+++ src/pages/Index/components/BeersList.tsx
@@ -0,0 +1,38 @@
+import { FC, useEffect, useMemo } from 'react';
+
+import { useBeers } from '../../../hooks/useBeers';
+
+import BeerCard from '../../../components/ui/BeerCard';
+
+interface Props {
+  search?: string;
+  page: number;
+  perPage: number;
+  ids?: number[];
+}
+
+const BeersList: FC<Props> = ({ search, page, perPage, ids }) => {
+  const beers = useBeers(page, perPage, search, ids);
+
+  const beerOpeningAudio = useMemo(() => new Audio('assets/beer-bottle-opening.mp3'), []);
+
+  useEffect(() => {
+    return () => {
+      beerOpeningAudio.pause();
+    };
+  }, [beerOpeningAudio]);
+
+  const handleBeerImageClick = () => (beerOpeningAudio.paused ? beerOpeningAudio.play() : {});
+
+  return (
+    <>
+      {beers.map(beer => (
+        <div key={beer.id} className="col-md-6 col-xl-4 d-flex justify-content-center">
+          <BeerCard beer={beer} onBeerImageClick={handleBeerImageClick} />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default BeersList;
