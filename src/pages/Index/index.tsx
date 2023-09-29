b79fbeed3833012a1a6e4428af6604a0d149903a
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-29 17:29:51 +0300

    feat: add BeersList in Index page

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,5 +1,26 @@
+import { useSearchParams } from 'react-router-dom';
+import BeersList from './components/BeersList';
+
 const Index = () => {
-  return <div>Home page</div>;
+  const [searchParams, setSearchParams] = useSearchParams({
+    page: '1',
+    per_page: '6',
+  });
+
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row gy-4 gx-5">
+          <BeersList
+            search=""
+            page={Number(searchParams.get('page')) || 1}
+            perPage={Number(searchParams.get('perPage')) || 6}
+          />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Index;
