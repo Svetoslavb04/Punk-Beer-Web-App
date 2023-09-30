5ec239baafb3fc986542713c45e1c137a7a9f3f7
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-10-01 01:01:19 +0300

    feat: add hashAll function

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,8 +1,14 @@
 export const hash = (val: any) =>
   crypto.subtle.digest('SHA-256', new TextEncoder().encode(JSON.stringify(val))).then(h => {
-    let hexes = [],
-      view = new DataView(h);
+    const hexes = [];
+    const view = new DataView(h);
     for (let i = 0; i < view.byteLength; i += 4)
       hexes.push(('00000000' + view.getUint32(i).toString(16)).slice(-8));
     return hexes.join('');
   });
+
+export const hashAll = async (val: any[]) => {
+  const allHashesPromises = val.map(async e => await hash(e));
+
+  return Promise.all(allHashesPromises);
+};
