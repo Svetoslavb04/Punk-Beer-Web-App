8be977a5471ac64790c53b1c25828732d500b88b
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-30 11:47:27 +0300

    fix: Index pass per page property to BeersList

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -14,7 +14,7 @@ import BeersList from '../../components/shared/BeersList';
 const Index = () => {
   const [searchParams, setSearchParams] = useSearchParams({
     page: '1',
-    per_page: '6',
+    perPage: '6',
   });
 
   const beerNameInput = useRef<HTMLInputElement>(null);
@@ -48,38 +48,36 @@ const Index = () => {
   const currentPage = Number(searchParams.get('page')) || 1;
 
   return (
-    <div>
-      <div className="container mt-5">
-        <div id="search-beer-container" className="my-4 mx-auto">
-          <InputGroup>
-            <FormControl
-              ref={beerNameInput}
-              placeholder="Search for beer..."
-              aria-label="Search for beer..."
-              aria-describedby="Search for beer"
-            />
-            <Button variant="outline-secondary" onClick={handleBeerSearch}>
-              Search
-            </Button>
-          </InputGroup>
-        </div>
-        <div className="row gy-4 gx-5">
-          <BeersList
-            search={searchParams.get('beerName') || ''}
-            page={Number(searchParams.get('page')) || 1}
-            perPage={Number(searchParams.get('perPage')) || 6}
+    <div className="container mt-5 flex-grow-1 d-flex flex-column">
+      <div id="search-beer-container" className="my-4 mx-auto">
+        <InputGroup>
+          <FormControl
+            ref={beerNameInput}
+            placeholder="Search for beer..."
+            aria-label="Search for beer..."
+            aria-describedby="Search for beer"
           />
-        </div>
-        <div id="beers-pagination" className="my-4 mx-auto">
-          <Pagination>
-            <Pagination.First onClick={() => handlePagination(1)} />
-            <Pagination.Prev
-              onClick={() => handlePagination(currentPage > 1 ? currentPage - 1 : 1)}
-            />
-            <Pagination.Item active>{currentPage}</Pagination.Item>
-            <Pagination.Next onClick={() => handlePagination(currentPage + 1)} />
-          </Pagination>
-        </div>
+          <Button variant="outline-secondary" onClick={handleBeerSearch}>
+            Search
+          </Button>
+        </InputGroup>
+      </div>
+      <div className="row gy-4 gx-5 flex-grow-1 align-items-center">
+        <BeersList
+          search={searchParams.get('beerName') || ''}
+          page={Number(searchParams.get('page')) || 1}
+          perPage={Number(searchParams.get('perPage')) || 6}
+        />
+      </div>
+      <div id="beers-pagination" className="my-4 mx-auto">
+        <Pagination>
+          <Pagination.First onClick={() => handlePagination(1)} />
+          <Pagination.Prev
+            onClick={() => handlePagination(currentPage > 1 ? currentPage - 1 : 1)}
+          />
+          <Pagination.Item active>{currentPage}</Pagination.Item>
+          <Pagination.Next onClick={() => handlePagination(currentPage + 1)} />
+        </Pagination>
       </div>
     </div>
   );
