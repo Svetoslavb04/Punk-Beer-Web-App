0c10ef8eb0434a8b43a33d10c3d0a52b2304d4c1
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-30 16:43:28 +0300

    feat: add test for useLocalStorage

---
+++ src/test/hooks/useLocalStorage.test.ts
@@ -0,0 +1,41 @@
+import { renderHook, act } from '@testing-library/react';
+
+import useLocalStorage from '../../hooks/useLocalStorage';
+
+describe('useLocalStorage', () => {
+  const initialState = { a: 1, b: 2 };
+
+  it('should return the initialState', () => {
+    const { result } = renderHook(() => useLocalStorage('bestObject', initialState));
+
+    expect(result.current[0]).toEqual(initialState);
+  });
+
+  it('should store the initialState as serialized json in localstorage', () => {
+    renderHook(() => useLocalStorage('bestObject', { a: 1, b: 2 }));
+
+    expect(localStorage.getItem('bestObject')).toEqual(JSON.stringify(initialState));
+  });
+
+  it('should return setValue that re-sets the value in local storage', () => {
+    const { result } = renderHook(() => useLocalStorage('bestObject', initialState));
+    const setItem = result.current[1];
+
+    const newValue = { a: 2, b: 5 };
+
+    act(() => {
+      setItem(newValue);
+    });
+
+    expect(result.current[0]).toEqual(newValue);
+    expect(localStorage.getItem('bestObject')).toEqual(JSON.stringify(newValue));
+  });
+
+  it('should set the value to the initial on error', async () => {
+    vi.spyOn(Storage.prototype, 'getItem').mockResolvedValueOnce('1,2');
+
+    const { result } = renderHook(() => useLocalStorage<any>('bestObject', initialState));
+
+    expect(result.current[0]).toEqual(initialState);
+  });
+});
