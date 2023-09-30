924da9435f97ea9e1d2fe0ed746eb2b04177298d
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-09-30 16:43:20 +0300

    feat: add useLocalStorage hook

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect, Dispatch } from 'react';
+
+export function useLocalStorage<T>(
+  key: string,
+  defaultValue: T,
+): [T, Dispatch<React.SetStateAction<T>>] {
+  const [value, setValue] = useState<T>(() => {
+    let currentValue = defaultValue;
+
+    try {
+      const item = localStorage.getItem(key);
+      if (item) {
+        currentValue = JSON.parse(item);
+      }
+    } catch (error) {}
+
+    return currentValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+}
+
+export default useLocalStorage;
