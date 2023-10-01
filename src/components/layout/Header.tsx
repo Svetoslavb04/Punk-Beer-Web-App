98155c92a8e915972aea4fac3c658fc08f7296b4
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-10-01 11:55:41 +0300

    feat: add connect/disconnet wallet button in Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,15 +1,48 @@
 import { Link, NavLink } from 'react-router-dom';
 
+import { useConnect, useAccount, useDisconnect } from 'wagmi';
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { sepolia } from 'wagmi/chains';
+
+import Button from 'react-bootstrap/Button';
+
 const Header = () => {
+  const connector = new MetaMaskConnector({
+    chains: [sepolia],
+    options: {
+      shimDisconnect: true,
+      UNSTABLE_shimOnConnectSelectAccount: true,
+    },
+  });
+
+  const { isConnected } = useAccount();
+  const { connect } = useConnect({
+    connector,
+  });
+  const { disconnect } = useDisconnect();
+
+  const handleConnectButtonClick = async () => {
+    if (!isConnected) {
+      await connect();
+    } else {
+      await disconnect();
+    }
+  };
+
   return (
-    <header className="w-100 d-flex justify-content-between align-items-center text-light">
+    <header className="w-100 d-flex flex-wrap justify-content-between align-items-center text-light">
       <Link to="/" className="fs-2 fw-light">
         Beans Love Beers
       </Link>
-      <nav className="d-flex align-items-center gap-4 fs-6">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/favourites">Favourites</NavLink>
-      </nav>
+      <div className="d-flex gap-5">
+        <Button variant="secondary" onClick={handleConnectButtonClick}>
+          {isConnected ? 'Disconnect' : 'Connect'} Wallet
+        </Button>
+        <nav className="d-flex align-items-center gap-4 fs-6">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/favourites">Favourites</NavLink>
+        </nav>
+      </div>
     </header>
   );
 };
