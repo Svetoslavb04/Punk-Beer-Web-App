7179f851751164c5fe134b716c7c7983d16d68d3
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-10-01 14:18:00 +0300

    feat: scroll to top on mount

--- src/hoc/withConnectedWallet.tsx
+++ src/hoc/withConnectedWallet.tsx
@@ -1,10 +1,20 @@
 import { useAccount } from 'wagmi';
 
 import Modal from 'react-bootstrap/Modal';
+import { useEffect } from 'react';
 
 export function withConnectedWallet<P extends object>(Component: React.ComponentType<P>) {
   return (props: P) => {
     const { isConnected } = useAccount();
+
+    useEffect(() => {
+      setTimeout(() => {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        });
+      }, 500);
+    }, []);
 
     return (
       <>
