37e31b50abdfe631af9f5b27545707e3f8872070
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2023-10-01 11:54:53 +0300

    Provide WagmiConfig to the App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,21 @@
 import { Outlet } from 'react-router-dom';
+
+import { WagmiConfig } from 'wagmi';
+import { config } from '../config/wagmi/wagmi';
+
 import Header from './layout/Header';
 
 function App() {
   return (
     <>
-      <div className="application">
-        <Header />
-        <main>
-          <Outlet />
-        </main>
-      </div>
+      <WagmiConfig config={config}>
+        <div className="application">
+          <Header />
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      </WagmiConfig>
     </>
   );
 }
